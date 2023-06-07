0cef4335f0faf2043ea0e648f4bfb75eb43b1ef8
DevSham <kjingo12@gmail.com>
2023-06-07 18:30:21 +0300

    - Create the restaurant crud routes

---
+++ routes/routes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+  const restaurants = require("../controllers/controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Restaurant
+  router.post("/", restaurants.create);
+
+  // Retrieve all Restaurants
+  router.get("/", restaurants.findAll);
+
+  // Retrieve all published Restaurants
+  router.get("/published", restaurants.findAllPublished);
+
+  // Retrieve a single Restaurant with id
+  router.get("/:id", restaurants.findOne);
+
+  // Update a Restaurant with id
+  router.put("/:id", restaurants.update);
+
+  // Delete a Restaurant with id
+  router.delete("/:id", restaurants.delete);
+
+  // Delete all Restaurants
+  router.delete("/", restaurants.deleteAll);
+
+  app.use('/api/restaurants', router);
+};
