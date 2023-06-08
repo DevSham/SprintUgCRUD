4f9b2ed3ce311cb966b0bc5eb94802a4572f92ca
DevSham <kjingo12@gmail.com>
2023-06-08 21:09:31 +0300

    Add more fields

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,24 +1,27 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+const Restaurant = db.restaurants;
 
-// Create and Save a new Tutorial
+// Create and Save a new Restaurant
 exports.create = (req, res) => {
     // Validate request
-    if (!req.body.title) {
+    if (!req.body.name) {
         res.status(400).send({ message: "Content can not be empty!" });
         return;
     }
 
-    // Create a Tutorial
-    const tutorial = new Tutorial({
-        title: req.body.title,
+    // Create a Restaurant
+    const restaurant = new Restaurant({
+        name: req.body.name,
+        location: req.body.location,
+        contact: req.body.contact,
+        cuisine_type: req.body.cuisine_type,
+        image_path: req.body.image_path,
         description: req.body.description,
-        published: req.body.published ? req.body.published : false
     });
 
-    // Save Tutorial in the database
-    tutorial
-        .save(tutorial)
+    // Save Restaurant in the database
+    restaurant
+        .save(restaurant)
         .then(data => {
             res.send(data);
         })
@@ -30,43 +33,43 @@ exports.create = (req, res) => {
         });
 };
 
-// Retrieve all Tutorials from the database.
+// Retrieve all Restaurants from the database.
 exports.findAll = (req, res) => {
     const title = req.query.title;
     var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 
-    Tutorial.find(condition)
+    Restaurant.find(condition)
         .then(data => {
             res.send(data);
         })
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving tutorials."
+                    err.message || "Some error occurred while retrieving restaurants."
             });
         });
 };
 
-// Find a single Tutorial with an id
+// Find a single Restaurant with an id
 exports.findOne = (req, res) => {
     const id = req.params.id;
 
-    Tutorial.findById(id)
+    Restaurant.findById(id)
         .then(data => {
             if (!data)
-                res.status(404).send({ message: "Not found Tutorial with id " + id });
+                res.status(404).send({ message: "Not found Restaurant with id " + id });
             else res.send(data);
         })
         .catch(err => {
             res
                 .status(500)
-                .send({ message: "Error retrieving Tutorial with id=" + id });
+                .send({ message: "Error retrieving Restaurant with id=" + id });
         });
 
 
 };
 
-// Update a Tutorial by the id in the request
+// Update a Restaurant by the id in the request
 exports.update = (req, res) => {
     if (!req.body) {
         return res.status(400).send({
@@ -76,74 +79,43 @@ exports.update = (req, res) => {
 
     const id = req.params.id;
 
-    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    Restaurant.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
         .then(data => {
             if (!data) {
                 res.status(404).send({
-                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+                    message: `Cannot update Restaurant with id=${id}. Maybe Restaurant was not found!`
                 });
-            } else res.send({ message: "Tutorial was updated successfully." });
+            } else res.send({ message: "Restaurant was updated successfully." });
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating Tutorial with id=" + id
+                message: "Error updating Restaurant with id=" + id
             });
         });
 
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a Restaurant with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
 
-    Tutorial.findByIdAndRemove(id)
+    Restaurant.findByIdAndRemove(id)
         .then(data => {
             if (!data) {
                 res.status(404).send({
-                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+                    message: `Cannot delete Restaurant with id=${id}. Maybe Restaurant was not found!`
                 });
             } else {
                 res.send({
-                    message: "Tutorial was deleted successfully!"
+                    message: "Restaurant was deleted successfully!"
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Could not delete Tutorial with id=" + id
+                message: "Could not delete Restaurant with id=" + id
             });
         });
 
 };
 
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {
-    Tutorial.deleteMany({})
-        .then(data => {
-            res.send({
-                message: `${data.deletedCount} Tutorials were deleted successfully!`
-            });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while removing all tutorials."
-            });
-        });
-
-};
-
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
-    Tutorial.find({ published: true })
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving tutorials."
-            });
-        });
-
-};
